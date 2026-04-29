f3de5fbeeff9d12ba78b9df6f61d5d8ef47d83d1
abhinavpanwar2511 <abhinavpanwar2511@gmail.com>
2026-04-29 11:50:02 +0530

    Update Main.java

--- LambdaExpression/Main.java
+++ LambdaExpression/Main.java
@@ -1,17 +1,110 @@
 package LambdaExpression;
 
 @FunctionalInterface
-interface Demo 
+// interface Demo 
+// {
+// 	void display();
+// }
+
+
+// public class Main {
+// 	public static void main(String[] args) 
+// 	{
+// 		Demo d = () -> { System.out.println("Welcome"); };
+		
+// 		d.display();
+// 	}
+
+// Or we can also write
+
+//interface Demo
+//{
+//	void display(int a);
+//}
+//
+//public class Main
+//{
+//	public static void main(String[] args) {
+//		Demo d = (int a) -> {System.out.println(a); };
+//		
+//		d.display(100);
+//	}
+//}
+
+//interface Demo
+//{
+//	void display(int a);
+//}
+//
+//public class Main
+//{
+//	public static void main(String[] args)
+//	{
+//		Demo d = (a) -> { System.out.println(a); };
+//		
+//		d.display(10);
+//	}
+//}
+
+//interface Demo
+//{
+//	void display(int a);
+//}
+//
+//public class Main
+//{
+//	public static void main(String[] args)
+//	{
+//		Demo d = a -> { System.out.println(a); };
+//		
+//		d.display(30);
+//	}
+//}
+
+//interface Demo
+//{
+//	void display(int a, int b);
+//}
+//
+//public class Main
+//{
+//	public static void main(String[] args)
+//	{
+//		Demo d = (a, b) -> { System.out.println(a + b); };
+//		
+//		d.display(30, 40);
+//	}
+//}
+
+//interface Demo
+//{
+//	int display(int a, int b);
+//}
+//
+//public class Main
+//{
+//	public static void main(String[] args)
+//	{
+//		Demo d = (a, b) -> { return a+b; };
+//		
+//		int res = d.display(100, 200);
+//		System.out.println(res);
+//	}
+//}
+
+interface Demo
 {
-	void display();
+	int display(int a, int b);
 }
 
-
-public class Main {
-	public static void main(String[] args) 
+public class Main
+{
+	public static void main(String[] args)
 	{
-		Demo d = () -> { System.out.println("Welcome"); };
+		Demo d = (a, b) -> a+b;
 		
-		d.display();
+		int res = d.display(30, 40);
+		System.out.println(res);
 	}
+	
 }

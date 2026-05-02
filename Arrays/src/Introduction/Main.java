25e65dc8f2e9f53b2bbf997dadd353e3642c9edc
abhinavpanwar2511 <abhinavpanwar2511@gmail.com>
2026-05-02 15:15:01 +0530

    Add files via upload

---
+++ Arrays/src/Introduction/Main.java
@@ -0,0 +1,15 @@
+package Introduction;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] marks = new int[5];
+		
+		marks[0] = 83;
+		marks[1] = 92;
+		marks[2] = 78;
+		marks[3] = 85;
+		marks[4] = 83;
+		
+		System.out.println(marks[4]);
+	}
+}
